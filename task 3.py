c4e3f36d935593cc284112d4c9d360119b1b1824
ShivamGupta86 <shivam515186@gmail.com>
2026-09-13 22:18:07 +0530

    Add Task 3 Basic Chatbot

---
+++ task 3.py
@@ -0,0 +1,22 @@
+# Task 4: Basic Chatbot
+
+print("===== Basic Chatbot =====")
+print("Bot: Hello! I am a simple chatbot.")
+print("Bot: You can say 'hello', 'how are you', or 'bye'.")
+print("Bot: Type 'bye' to exit.\n")
+
+while True:
+    user_input = input("You: ").lower()
+
+    if user_input == "hello":
+        print("Bot: Hi!")
+
+    elif user_input == "how are you":
+        print("Bot: I'm fine, thanks!")
+
+    elif user_input == "bye":
+        print("Bot: Goodbye!")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand that.")
